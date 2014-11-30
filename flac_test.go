58d7c0ba5e14563f12665095b72a91a8658442ad
Simon Allen <simon@simonallen.org>
2014-11-30 13:04:54 +1100

    fix assert.Equal arg order

--- flac_test.go
+++ flac_test.go
@@ -20,25 +20,25 @@ func (suite *FLACTestSuite) SetupTest() {
 	suite.flac, err = Parse("sample.flac")
 
 	suite.NoError(err)
-	suite.assert.Equal(suite.flac.Marker, "fLaC")
+	suite.assert.Equal("fLaC", suite.flac.Marker)
 }
 
 func (suite *FLACTestSuite) TestFLACMetadataBlockStreamInfo() {
 	suite.assert.NotNil(suite.flac.StreamInfo)
-	suite.assert.Equal(suite.flac.StreamInfo.FLACMetadataBlock.FLAC, suite.flac)
+	suite.assert.Equal(suite.flac, suite.flac.StreamInfo.FLACMetadataBlock.FLAC)
 	suite.assert.False(suite.flac.StreamInfo.FLACMetadataBlock.Last)
-	suite.assert.Equal(suite.flac.StreamInfo.FLACMetadataBlock.Type, StreamInfo)
-	suite.assert.Equal(suite.flac.StreamInfo.FLACMetadataBlock.DataLength, 34)
-	suite.assert.Equal(suite.flac.StreamInfo.MinBlockSize, 4096)
-	suite.assert.Equal(suite.flac.StreamInfo.MaxBlockSize, 4096)
-	suite.assert.Equal(suite.flac.StreamInfo.MinFrameSize, 7822)
-	suite.assert.Equal(suite.flac.StreamInfo.MaxFrameSize, 17848)
-	suite.assert.Equal(suite.flac.StreamInfo.SampleRate, 88200)
-	suite.assert.Equal(suite.flac.StreamInfo.Channels, 2)
-	suite.assert.Equal(suite.flac.StreamInfo.BitsPerSample, 24)
-	suite.assert.Equal(suite.flac.StreamInfo.NumSamples, 793287)
-	suite.assert.Equal(hex.EncodeToString(suite.flac.StreamInfo.UnencodedMD5),
-		"29499b5e67ae77df6f8491329c4deb93")
+	suite.assert.Equal(StreamInfo, suite.flac.StreamInfo.FLACMetadataBlock.Type)
+	suite.assert.Equal(34, suite.flac.StreamInfo.FLACMetadataBlock.DataLength)
+	suite.assert.Equal(4096, suite.flac.StreamInfo.MinBlockSize)
+	suite.assert.Equal(4096, suite.flac.StreamInfo.MaxBlockSize)
+	suite.assert.Equal(7822, suite.flac.StreamInfo.MinFrameSize)
+	suite.assert.Equal(17848, suite.flac.StreamInfo.MaxFrameSize)
+	suite.assert.Equal(88200, suite.flac.StreamInfo.SampleRate)
+	suite.assert.Equal(2, suite.flac.StreamInfo.Channels)
+	suite.assert.Equal(24, suite.flac.StreamInfo.BitsPerSample)
+	suite.assert.Equal(793287, suite.flac.StreamInfo.NumSamples)
+	suite.assert.Equal("29499b5e67ae77df6f8491329c4deb93",
+		hex.EncodeToString(suite.flac.StreamInfo.UnencodedMD5))
 }
 
 func (suite *FLACTestSuite) TestFLACMetadataBlockSeekTable() {
@@ -53,17 +53,17 @@ func (suite *FLACTestSuite) TestFLACMetadataBlockSeekTable() {
 
 		testedBlocks++
 
-		suite.assert.Equal(block.FLACMetadataBlock.FLAC, suite.flac)
+		suite.assert.Equal(suite.flac, block.FLACMetadataBlock.FLAC)
 		suite.assert.False(block.FLACMetadataBlock.Last)
-		suite.assert.Equal(block.FLACMetadataBlock.Type, SeekTable)
-		suite.assert.Equal(block.FLACMetadataBlock.DataLength, 18)
-		suite.assert.Equal(len(block.SeekPoints), 1)
-		suite.assert.Equal(block.SeekPoints[0].Sample, 0)
-		suite.assert.Equal(block.SeekPoints[0].ByteOffset, 0)
-		suite.assert.Equal(block.SeekPoints[0].NumSamples, 4096)
+		suite.assert.Equal(SeekTable, block.FLACMetadataBlock.Type)
+		suite.assert.Equal(18, block.FLACMetadataBlock.DataLength)
+		suite.assert.Equal(1, len(block.SeekPoints))
+		suite.assert.Equal(0, block.SeekPoints[0].Sample)
+		suite.assert.Equal(0, block.SeekPoints[0].ByteOffset)
+		suite.assert.Equal(4096, block.SeekPoints[0].NumSamples)
 	}
 
-	suite.assert.Equal(testedBlocks, 1)
+	suite.assert.Equal(1, testedBlocks)
 }
 
 func (suite *FLACTestSuite) TestFLACMetadataBlockApplication() {
@@ -78,15 +78,15 @@ func (suite *FLACTestSuite) TestFLACMetadataBlockApplication() {
 
 		testedBlocks++
 
-		suite.assert.Equal(block.FLACMetadataBlock.FLAC, suite.flac)
+		suite.assert.Equal(suite.flac, block.FLACMetadataBlock.FLAC)
 		suite.assert.False(block.FLACMetadataBlock.Last)
-		suite.assert.Equal(block.FLACMetadataBlock.Type, Application)
-		suite.assert.Equal(block.FLACMetadataBlock.DataLength, 8)
-		suite.assert.Equal(block.AppId, "ATCH")
-		suite.assert.Equal(string(block.AppData), "C@K3")
+		suite.assert.Equal(Application, block.FLACMetadataBlock.Type)
+		suite.assert.Equal(8, block.FLACMetadataBlock.DataLength)
+		suite.assert.Equal("ATCH", block.AppId)
+		suite.assert.Equal("C@K3", string(block.AppData))
 	}
 
-	suite.assert.Equal(testedBlocks, 1)
+	suite.assert.Equal(1, testedBlocks)
 }
 
 func (suite *FLACTestSuite) TestFLACMetadataBlockVorbisComment() {
@@ -101,21 +101,21 @@ func (suite *FLACTestSuite) TestFLACMetadataBlockVorbisComment() {
 
 		testedBlocks++
 
-		suite.assert.Equal(block.FLACMetadataBlock.FLAC, suite.flac)
+		suite.assert.Equal(suite.flac, block.FLACMetadataBlock.FLAC)
 		suite.assert.False(block.FLACMetadataBlock.Last)
-		suite.assert.Equal(block.FLACMetadataBlock.Type, VorbisComment)
-		suite.assert.Equal(block.FLACMetadataBlock.DataLength, 56)
-		suite.assert.Equal(block.VendorString, "reference libFLAC 1.1.4 20070213")
-		suite.assert.Equal(len(block.Comments), 1)
+		suite.assert.Equal(VorbisComment, block.FLACMetadataBlock.Type)
+		suite.assert.Equal(56, block.FLACMetadataBlock.DataLength)
+		suite.assert.Equal("reference libFLAC 1.1.4 20070213", block.VendorString)
+		suite.assert.Equal(1, len(block.Comments))
 
 		value, ok := block.Comments["example"]
 
 		suite.assert.True(ok)
-		suite.assert.Equal(len(value), 1)
+		suite.assert.Equal(1, len(value))
 		suite.assert.Contains(value, "fish")
 	}
 
-	suite.assert.Equal(testedBlocks, 1)
+	suite.assert.Equal(1, testedBlocks)
 }
 
 func (suite *FLACTestSuite) TestFLACMetadataBlockPicture() {
@@ -130,21 +130,21 @@ func (suite *FLACTestSuite) TestFLACMetadataBlockPicture() {
 
 		testedBlocks++
 
-		suite.assert.Equal(block.FLACMetadataBlock.FLAC, suite.flac)
+		suite.assert.Equal(suite.flac, block.FLACMetadataBlock.FLAC)
 		suite.assert.False(block.FLACMetadataBlock.Last)
-		suite.assert.Equal(block.FLACMetadataBlock.Type, Picture)
-		suite.assert.Equal(block.FLACMetadataBlock.DataLength, 1661438)
-		suite.assert.Equal(block.Type, FrontCover)
-		suite.assert.Equal(block.MIMEType, "image/jpeg")
-		suite.assert.Equal(block.Description, "")
-		suite.assert.Equal(block.Width, 2448)
-		suite.assert.Equal(block.Height, 3264)
-		suite.assert.Equal(block.ColourDepth, 24)
-		suite.assert.Equal(block.NumColours, 0)
+		suite.assert.Equal(Picture, block.FLACMetadataBlock.Type)
+		suite.assert.Equal(1661438, block.FLACMetadataBlock.DataLength)
+		suite.assert.Equal(FrontCover, block.Type)
+		suite.assert.Equal("image/jpeg", block.MIMEType)
+		suite.assert.Equal("", block.Description)
+		suite.assert.Equal(2448, block.Width)
+		suite.assert.Equal(3264, block.Height)
+		suite.assert.Equal(24, block.ColourDepth)
+		suite.assert.Equal(0, block.NumColours)
 		suite.assert.Equal("c6f3cec420be726d74ca3ccfb7461f65", hex.EncodeToString(block.PictureMD5))
 	}
 
-	suite.assert.Equal(testedBlocks, 1)
+	suite.assert.Equal(1, testedBlocks)
 }
 
 func (suite *FLACTestSuite) TestFLACMetadataBlockCueSheet() {
@@ -159,11 +159,11 @@ func (suite *FLACTestSuite) TestFLACMetadataBlockCueSheet() {
 
 		testedBlocks++
 
-		suite.assert.Equal(block.FLACMetadataBlock.FLAC, suite.flac)
+		suite.assert.Equal(suite.flac, block.FLACMetadataBlock.FLAC)
 		suite.assert.False(block.FLACMetadataBlock.Last)
-		suite.assert.Equal(block.FLACMetadataBlock.Type, CueSheet)
-		suite.assert.Equal(block.FLACMetadataBlock.DataLength, 576)
-		suite.assert.Equal(len(block.MediaCatalogNumber), 128)
+		suite.assert.Equal(CueSheet, block.FLACMetadataBlock.Type)
+		suite.assert.Equal(576, block.FLACMetadataBlock.DataLength)
+		suite.assert.Equal(128, len(block.MediaCatalogNumber))
 
 		for _, c := range block.MediaCatalogNumber {
 			suite.assert.Equal('\x00', rune(c))
@@ -175,7 +175,7 @@ func (suite *FLACTestSuite) TestFLACMetadataBlockCueSheet() {
 
 		suite.assert.Equal(0, block.CueSheetTracks[0].Offset)
 		suite.assert.Equal(1, block.CueSheetTracks[0].Track)
-		suite.assert.Equal(len(block.CueSheetTracks[0].ISRC), 12)
+		suite.assert.Equal(12, len(block.CueSheetTracks[0].ISRC))
 
 		for _, c := range block.CueSheetTracks[0].ISRC {
 			suite.assert.Equal('\x00', rune(c))
@@ -189,7 +189,7 @@ func (suite *FLACTestSuite) TestFLACMetadataBlockCueSheet() {
 
 		suite.assert.Equal(3528, block.CueSheetTracks[1].Offset)
 		suite.assert.Equal(2, block.CueSheetTracks[1].Track)
-		suite.assert.Equal(len(block.CueSheetTracks[0].ISRC), 12)
+		suite.assert.Equal(12, len(block.CueSheetTracks[0].ISRC))
 
 		for _, c := range block.CueSheetTracks[0].ISRC {
 			suite.assert.Equal('\x00', rune(c))
@@ -203,7 +203,7 @@ func (suite *FLACTestSuite) TestFLACMetadataBlockCueSheet() {
 
 		suite.assert.Equal(4704, block.CueSheetTracks[2].Offset)
 		suite.assert.Equal(3, block.CueSheetTracks[2].Track)
-		suite.assert.Equal(len(block.CueSheetTracks[2].ISRC), 12)
+		suite.assert.Equal(12, len(block.CueSheetTracks[2].ISRC))
 
 		for _, c := range block.CueSheetTracks[2].ISRC {
 			suite.assert.Equal('\x00', rune(c))
@@ -217,7 +217,7 @@ func (suite *FLACTestSuite) TestFLACMetadataBlockCueSheet() {
 
 		suite.assert.Equal(793287, block.CueSheetTracks[3].Offset)
 		suite.assert.Equal(255, block.CueSheetTracks[3].Track)
-		suite.assert.Equal(len(block.CueSheetTracks[3].ISRC), 12)
+		suite.assert.Equal(12, len(block.CueSheetTracks[3].ISRC))
 
 		for _, c := range block.CueSheetTracks[3].ISRC {
 			suite.assert.Equal('\x00', rune(c))
@@ -228,7 +228,7 @@ func (suite *FLACTestSuite) TestFLACMetadataBlockCueSheet() {
 		suite.assert.Equal(0, len(block.CueSheetTracks[3].CueSheetTrackIndices))
 	}
 
-	suite.assert.Equal(testedBlocks, 1)
+	suite.assert.Equal(1, testedBlocks)
 }
 
 func (suite *FLACTestSuite) TestFLACMetadataBlockPadding() {
@@ -243,14 +243,14 @@ func (suite *FLACTestSuite) TestFLACMetadataBlockPadding() {
 
 		testedBlocks++
 
-		suite.assert.Equal(block.FLACMetadataBlock.FLAC, suite.flac)
+		suite.assert.Equal(suite.flac, block.FLACMetadataBlock.FLAC)
 		suite.assert.True(block.FLACMetadataBlock.Last)
-		suite.assert.Equal(block.FLACMetadataBlock.Type, Padding)
-		suite.assert.Equal(block.FLACMetadataBlock.DataLength, 7596)
-		suite.assert.Equal(block.NumBytes, 7596)
+		suite.assert.Equal(Padding, block.FLACMetadataBlock.Type)
+		suite.assert.Equal(7596, block.FLACMetadataBlock.DataLength)
+		suite.assert.Equal(7596, block.NumBytes)
 	}
 
-	suite.assert.Equal(testedBlocks, 1)
+	suite.assert.Equal(1, testedBlocks)
 }
 
 func TestFLACTestSuite(t *testing.T) {
